db41d42a6e15d19ce1b00f9161706f7a7d6a27c6
DebajitMondal <73100698+DebajitMondal@users.noreply.github.com>
2020-11-14 09:47:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,BOX1,BOX2,BOX3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var box1,box2,box3;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	box1= createSprite(width/2,650,100,20)
+	box1.shapeColor="red";
+
+	box2= createSprite(width/2-60,620,20,80)
+	box2.shapeColor="red";
+
+	box3= createSprite(width/2+60,620,20,80)
+	box3.shapeColor="red";
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:false});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+  BOX1 = Bodies.rectangle(width/2, 630, width, 10 , {isStatic:true} )
+  World.add(world, BOX1);
+  BOX2 = Bodies.rectangle(width/2-60, 620, 10, height , {isStatic:true} )
+  World.add(world, BOX2);
+  BOX3 = Bodies.rectangle(width/2+60, 620, 10, height , {isStatic:true} )
+  World.add(world, BOX3);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+ 
+  	rectMode(CENTER)
+	rect(packageBody.position.x, packageBody.position.y, 20,20) 
+  drawSprites();
+ 
+}
+
+
+
+
